d558c8488a70890cae8ea67db2a567c1008101a1
anush-malik32 <malik.anush1@gmail.com>
2026-02-01 10:02:14 -0500

    Pushing Python File

---
+++ Helloworld.py
@@ -0,0 +1,4 @@
+#print("Hi, My name is Kwado")
+print("Hi, My name is Anush")
+print("i today in python and my teacher got sick. \nI like Python")
+print("Here I begin a tab space, \tNow do you see the gap ?")
