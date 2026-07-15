d1d2a00289859bafaffe68468d14cd95d51db98f
anush-malik32 <malik.anush1@gmail.com>
2026-07-15 12:41:21 -0400

    Today we worked on secret agent

---
+++ string.py
@@ -0,0 +1,10 @@
+vowels = "AEIOUaeiou"
+print(vowels [0:len (vowels):1])
+print(vowels [:len (vowels):1])
+print(vowels [0::1])
+print(vowels [0: len (vowels)])
+
+print(vowels[::-1])
+print(vowels[-5::])
+print(vowels[-6::-1])
+print(vowels[-9::2])
